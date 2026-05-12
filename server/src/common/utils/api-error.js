dea2228e7864dbcbf414f9c4c88ebe657deefdec
omkarg419 <68603231+omkarg419@users.noreply.github.com>
2026-05-12 16:58:02 +0530

    feat: enhance ApiError class to include details in error responses

--- server/src/common/utils/api-error.js
+++ server/src/common/utils/api-error.js
@@ -1,27 +1,34 @@
 class ApiError extends Error {
-  constructor(statusCode, message) {
-    super(message);
-    this.statusCode = statusCode;
-    // this.isOperational = true;
-    Error.captureStackTrace(this, this.constructor);
-  }
+	constructor(statusCode, message, details = null) {
+		super(message);
+		this.statusCode = statusCode;
+		this.details = details;
+		Error.captureStackTrace(this, this.constructor);
+	}
 
-  static badRequest(message = "Bad request") {
-    return new ApiError(400, message);
-  }
+	static badRequest(message = "Bad request", details = null) {
+		return new ApiError(400, message, details);
+	}
 
-  static unauthorized(message = "Unauthorized") {
-    return new ApiError(401, message);
-  }
-  static conflict(message = "Conflict") {
-    return new ApiError(409, message);
-  }
-  static forbidden(message = "forbidden") {
-    return new ApiError(412, message);
-  }
-  static notfound(message = "notfound") {
-    return new ApiError(412, message);
-  }
+	static unauthorized(message = "Unauthorized", details = null) {
+		return new ApiError(401, message, details);
+	}
+
+	static forbidden(message = "Forbidden", details = null) {
+		return new ApiError(403, message, details);
+	}
+
+	static notFound(message = "Not found", details = null) {
+		return new ApiError(404, message, details);
+	}
+
+	static conflict(message = "Conflict", details = null) {
+		return new ApiError(409, message, details);
+	}
+
+	static unprocessableEntity(message = "Unprocessable entity", details = null) {
+		return new ApiError(422, message, details);
+	}
 }
 
 export default ApiError;
